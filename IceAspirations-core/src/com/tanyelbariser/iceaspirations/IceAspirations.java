1289fadd93d73b8bb5f8f813b9d54cf6fe7d712c
Tanyel Bariser <tanyel19@hotmail-com>
2014-07-19 20:48:43 +0100

    Main class extends Game, add CartoonSky.png & render to screen portrait

--- IceAspirations-core/src/com/tanyelbariser/iceaspirations/IceAspirations.java
+++ IceAspirations-core/src/com/tanyelbariser/iceaspirations/IceAspirations.java
@@ -1,19 +1,23 @@
 package com.tanyelbariser.iceaspirations;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class IceAspirations extends ApplicationAdapter {
+public class IceAspirations extends Game {
 	SpriteBatch batch;
-	Texture img;
+	Texture texture;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("Cartoon Sky.jpg");
+		sprite = new Sprite(texture);
+		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
@@ -21,7 +25,7 @@ public class IceAspirations extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 }
