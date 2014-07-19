da58abad6f5983d775b9cecfe81e498278063294
Tanyel Bariser <tanyel19@hotmail-com>
2014-07-19 15:23:29 +0100

    Initial Commit with Default BadLogic Texture on Build

---
+++ IceAspirations-desktop/src/com/tanyelbariser/iceaspirations/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tanyelbariser.iceaspirations.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tanyelbariser.iceaspirations.IceAspirations;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IceAspirations(), config);
+	}
+}
