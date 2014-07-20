262cdfd25a0cd37abe39047609d099fb8229d40f
Tanyel Bariser <tanyel19@hotmail-com>
2014-07-20 18:06:51 +0100

    Created MainScreen class in new Screens package & resized image

--- IceAspirations-core/src/com/tanyelbariser/iceaspirations/IceAspirations.java
+++ IceAspirations-core/src/com/tanyelbariser/iceaspirations/IceAspirations.java
@@ -1,31 +1,13 @@
 package com.tanyelbariser.iceaspirations;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.tanyelbariser.iceaspirations.screens.MainScreen;
 
 public class IceAspirations extends Game {
-	SpriteBatch batch;
-	Texture texture;
-	Sprite sprite;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		texture = new Texture("Cartoon Sky.jpg");
-		sprite = new Sprite(texture);
-		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		setScreen(new MainScreen());
 	}
 }
---
+++ IceAspirations-core/src/com/tanyelbariser/iceaspirations/screens/MainScreen.java
@@ -0,0 +1,62 @@
+package com.tanyelbariser.iceaspirations.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainScreen implements Screen {
+	SpriteBatch batch;
+	Texture texture;
+	Sprite sprite;
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		batch = new SpriteBatch();
+		texture = new Texture("Cartoon Sky.jpg");
+		sprite = new Sprite(texture);
+		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
