2472a6fe4d58120ce68bed826227bd80daf67a6b
Tanyel Bariser <tanyel19@hotmail-com>
2014-07-22 17:30:43 +0100

    Constructor for MainScreen(IceAspirations) created MockScreen and Icon

--- IceAspirations-core/src/com/tanyelbariser/iceaspirations/IceAspirations.java
+++ IceAspirations-core/src/com/tanyelbariser/iceaspirations/IceAspirations.java
@@ -1,60 +1,11 @@
 package com.tanyelbariser.iceaspirations;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.tanyelbariser.iceaspirations.screens.MainScreen;
 
 public class IceAspirations extends Game {
-	//Will get rid of logging information later,
-	// just here to get me used to app lifecycle
-	final String TAG = "IN ICEASPIRATIONS";
-	
 	@Override
 	public void create() {
-		Gdx.app.log(TAG, "create()");
-		setScreen(new MainScreen());
-	}
-
-	@Override
-	public void setScreen(Screen screen) {
-		Gdx.app.log(TAG, "setScreen(Screen screen)");
-		super.setScreen(screen);
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		Gdx.app.log(TAG, "resize(int width, int height)");
-		super.resize(width, height);
-	}
-
-	@Override
-	public void render() {
-		Gdx.app.log(TAG, "render()");
-		super.render();
-	}
-
-	@Override
-	public void pause() {
-		Gdx.app.log(TAG, "pause()");
-		super.pause();
-	}
-	
-	@Override
-	public void dispose() {
-		Gdx.app.log(TAG, "dipose()");
-		super.dispose();
-	}
-
-	@Override
-	public void resume() {
-		Gdx.app.log(TAG, "resume()");
-		super.resume();
-	}
-
-	@Override
-	public Screen getScreen() {
-		Gdx.app.log(TAG, "getScreen()");
-		return super.getScreen();
+		setScreen(new MainScreen(this));
 	}
 }
--- IceAspirations-core/src/com/tanyelbariser/iceaspirations/screens/MainScreen.java
+++ IceAspirations-core/src/com/tanyelbariser/iceaspirations/screens/MainScreen.java
@@ -6,28 +6,30 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.tanyelbariser.iceaspirations.IceAspirations;
 
 public class MainScreen implements Screen {
+	IceAspirations iceA;
 	SpriteBatch batch;
 	Sprite sprite;
-	final String TAG = "IN MAINSCREEN"; 
 
+	public MainScreen(IceAspirations iceA) {
+		this.iceA = iceA;
+	}
+	
 	@Override
 	public void show() {
-		Gdx.app.log(TAG, "show()");
 		batch = new SpriteBatch();
-		sprite = new Sprite(new Texture("Cartoon Sky.jpg"));
+		sprite = new Sprite(new Texture("Mock Screen.jpg"));
 		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());		
 	}
 	
 	@Override
 	public void resize(int width, int height) {
-		Gdx.app.log(TAG, "resize(int width, int height)");	
 	}
 	
 	@Override
 	public void render(float delta) {
-		Gdx.app.log(TAG, "render(float delta)");
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
@@ -37,24 +39,20 @@ public class MainScreen implements Screen {
 
 	@Override
 	public void pause() {
-		Gdx.app.log(TAG, "pause()");
 	}
 
 	@Override
 	public void hide() {
-		Gdx.app.log(TAG, "hide()");
 		dispose();	
 	}
 
 	@Override
 	public void dispose() {
-		Gdx.app.log(TAG, "dipose()");
 		batch.dispose();
 		sprite.getTexture().dispose();
 	}
 	
 	@Override
 	public void resume() {
-		Gdx.app.log(TAG, "resume()");
 	}
 }
