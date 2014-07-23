1544d20a2479f28bca079eec34f34b0d84459b09
Tanyel Bariser <tanyel19@hotmail-com>
2014-07-23 19:39:34 +0100

    Created Play imageTextButton in MainScreen, added new font to assets

--- IceAspirations-core/src/com/tanyelbariser/iceaspirations/screens/MainScreen.java
+++ IceAspirations-core/src/com/tanyelbariser/iceaspirations/screens/MainScreen.java
@@ -4,30 +4,49 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton.ImageTextButtonStyle;
 import com.tanyelbariser.iceaspirations.IceAspirations;
 
 public class MainScreen implements Screen {
 	IceAspirations iceA;
 	SpriteBatch batch;
 	Sprite sprite;
+	BitmapFont blue;
+	Stage stage;
+	ImageTextButtonStyle style;
+	ImageTextButton play, highscores, quit;
 
 	public MainScreen(IceAspirations iceA) {
 		this.iceA = iceA;
 	}
-	
+
 	@Override
 	public void show() {
+		stage = new Stage();
 		batch = new SpriteBatch();
-		sprite = new Sprite(new Texture("Mock Screen.jpg"));
-		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());		
+		sprite = new Sprite(new Texture("Main Screen.png"));
+		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		blue = new BitmapFont(Gdx.files.internal("blue.fnt"), false);
+		style = new ImageTextButtonStyle();
+		style.font = blue;
+
+		//Create play button in main screen to transition to game screen
+		play = new ImageTextButton("Play", style);
+		float playX = Gdx.graphics.getWidth() / 2 - play.getWidth() / 2;
+		float playY = Gdx.graphics.getHeight() / 2 - play.getHeight() / 2;
+		play.setPosition(playX, playY);
+		stage.addActor(play);
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
@@ -35,6 +54,9 @@ public class MainScreen implements Screen {
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
+
+		stage.act(delta);
+		stage.draw();
 	}
 
 	@Override
@@ -43,15 +65,19 @@ public class MainScreen implements Screen {
 
 	@Override
 	public void hide() {
-		dispose();	
+		dispose();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
 		sprite.getTexture().dispose();
+		blue.dispose();
+		stage.dispose();
+		
+		
 	}
-	
+
 	@Override
 	public void resume() {
 	}
